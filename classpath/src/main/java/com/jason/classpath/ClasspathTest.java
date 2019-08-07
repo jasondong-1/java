f0cc3ef981fd96b3ea14374340aacedc2c87710f
jason <745650624@qq.com>
2019-08-07 14:46:41 +0800

    添加了classpath的测试

--- classpath/src/main/java/com/jason/classpath/ClasspathTest.java
+++ classpath/src/main/java/com/jason/classpath/ClasspathTest.java
@@ -1,12 +1,19 @@
 package com.jason.classpath;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 
 public class ClasspathTest {
 
     public ClasspathTest() throws URISyntaxException {
+
+    }
+
+    public void test1(){
         URL url = getClass().getClassLoader().getResource("");
         System.out.println(url.getFile());
         File file = new File(url.getFile());
@@ -15,7 +22,18 @@ public class ClasspathTest {
         }
     }
 
-    public static void main(String[] args) throws URISyntaxException {
-        new ClasspathTest();
+    public void test2() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("application.yml")));
+        String line = null;
+        while ((line=reader.readLine())!=null) {
+            System.out.println(line);
+        }
+        reader.close();
+    }
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        ClasspathTest ct =  new ClasspathTest();
+        ct.test1();
+        ct.test2();
     }
 }
