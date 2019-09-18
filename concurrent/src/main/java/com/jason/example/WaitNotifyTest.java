ea7c1dd539c9a1872bdf32effcb295a85670067a
jason <745650624@qq.com>
2019-09-18 11:15:13 +0800

    临时提交

---
+++ concurrent/src/main/java/com/jason/example/WaitNotifyTest.java
@@ -0,0 +1,79 @@
+package com.jason.example;
+
+public class WaitNotifyTest {
+    private boolean boo;
+
+    public WaitNotifyTest() {
+    }
+
+    public boolean isBoo() {
+        return boo;
+    }
+
+    public void setBoo(boolean boo) {
+        synchronized (this) {
+            this.boo = boo;
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //this.notify();
+            this.notifyAll();
+        }
+    }
+
+    public void print() {
+        synchronized (this) {
+            while (!isBoo()) {
+                try {
+                    System.out.println(Thread.currentThread().getName()+ " 条件不满足，歇息一会儿");
+                    this.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + " : " + getClass().getSimpleName());
+        }
+    }
+
+    public static void main(String[] args) {
+        WaitNotifyTest waitNotifyTest = new WaitNotifyTest();
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(new WaitRunable(waitNotifyTest), WaitNotifyTest.class.getSimpleName() + i);
+            thread.start();
+        }
+        Thread thread = new Thread(new NotifyRunnable(waitNotifyTest, true));
+        thread.start();
+    }
+
+}
+
+
+class WaitRunable implements Runnable {
+    private WaitNotifyTest waitNotifyTest;
+
+    public WaitRunable(WaitNotifyTest waitNotifyTest) {
+        this.waitNotifyTest = waitNotifyTest;
+    }
+
+    @Override
+    public void run() {
+        waitNotifyTest.print();
+    }
+}
+
+class NotifyRunnable implements Runnable {
+    private WaitNotifyTest waitNotifyTest;
+    private boolean flag;
+
+    public NotifyRunnable(WaitNotifyTest waitNotifyTest, boolean flag) {
+        this.waitNotifyTest = waitNotifyTest;
+        this.flag = flag;
+    }
+
+    @Override
+    public void run() {
+        waitNotifyTest.setBoo(flag);
+    }
+}
