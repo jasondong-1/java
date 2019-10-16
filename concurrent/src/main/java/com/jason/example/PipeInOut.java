91d2954c5800853190032e29af9b608a678cee84
jason <745650624@qq.com>
2019-10-16 16:56:57 +0800

    20191016添加了一些例子

---
+++ concurrent/src/main/java/com/jason/example/PipeInOut.java
@@ -0,0 +1,47 @@
+package com.jason.example;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.nio.charset.Charset;
+
+public class PipeInOut {
+    public static void main(String[] args) throws IOException {
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        Thread thread = new Thread(new PipeRunnable(out));
+        thread.start();
+        byte[] bytes = new byte[1024];
+        int len = -1;
+        while ((len = in.read(bytes)) != -1) {
+            System.out.println(new String(bytes, 0, len, "utf-8"));
+        }
+        in.close();
+    }
+}
+
+class PipeRunnable implements Runnable {
+    private PipedOutputStream out;
+
+    public PipeRunnable(PipedOutputStream out) {
+        this.out = out;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            String s = new String("jason" + i);
+            try {
+                out.write(s.getBytes(Charset.forName("utf-8")));
+                Thread.sleep(1000);
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
